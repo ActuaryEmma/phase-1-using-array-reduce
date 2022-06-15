43e52372769e2d3a25cbe8490611b61cf74dac5b
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-15 09:01:23 +0300

    commit tree 8f9815ca7e469746a5b99ae6e09d723f71e9a83e

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries= batteryBatches.reduce(function(total,batteries){
+  return total + batteries;
+});
+console.log(totalBatteries);
 
-// Code your solution here
